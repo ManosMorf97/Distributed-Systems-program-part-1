43164f8f1a2a127cbd3f8e49ebd467b8d5c70e53
DrazorV <vagelisp.97@gmail.com>
2019-03-15 15:56:43 +0200

    Changed code

--- Publisher.java
+++ Publisher.java
@@ -1,8 +1,18 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 class Publisher{
 
+    private static HashMap<String, BusAndLocation> Keys;
+    private ArrayList<Publisher> registeredPublishers;
 
 
+    private Publisher(ArrayList<Broker> brokers) {
 
+    }
 
+    private HashMap<String,BusAndLocation> getKeys(){
+        return Keys;
+    }
 
 }
