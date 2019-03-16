dc9e48d003a009cbd2b1db56182f342a731f451f
Morfiadakis Emmanouil <48352519+ManosMorf22@users.noreply.github.com>
2019-03-16 16:01:58 +0200

    Update

--- Broker.java
+++ Broker.java
@@ -4,22 +4,21 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
+
 
 //lol
-public class Broker implements Runnable{
+public class Broker{
 	private DataInputStream dis;
 	private DataOutputStream dos;
 	private Socket s;
-	private int hashnumber;
-	private String hashstring;
+	private static int hashnumber;
+	private static String hashstring;
 	private static boolean recieveorsend = false;
 	//we want the data from publisher
 	private static ArrayList<BusAndLocation> DataFromPublisher;
 	private static ArrayList<BusAndLocation> DataResponsible;
-	private static ArrayList<Consumer> consumers;
-	private static ArrayList<Publisher> registeredPublishers;
-	private Iterator registeredPublishersIterator = null;
+	protected static ArrayList<Consumer> consumers;
+	protected static ArrayList<Publisher> registeredPublishers;
 	private static HashMap<String, BusAndLocation> Keys;
 	private static int PublisherID = -1;//UNDONE
 	private static int ConsumerID = -1;//UNDONE
@@ -47,7 +46,7 @@ public class Broker implements Runnable{
 		return Keys;
 	}
 
-	private int gethashnumber(){
+	private static int gethashnumber(){
 		return hashnumber;
 	}
 
@@ -55,122 +54,109 @@ public class Broker implements Runnable{
 
 	}
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException,InterruptedException {
 
 		// server is listening on port 5056
-		ServerSocket serverSocket = new ServerSocket(5056);
-		while (true)
-		{
-			Socket socket = null;
+		Thread [] pubthread=new Thread[registeredPublishers.size()];
+		for(int i=0; i<registeredPublishers.size(); i++){
+			pubthread[i]=new Thread(new PubBroker(i));
+			pubthread[i].start();
+		}
+		for(int i=0; i<registeredPublishers.size(); i++){
+			pubthread[i].join();
+		}
+		Thread [] conthread=new Thread[consumers.size()];
+		for(int i=0; i<consumers.size(); i++){
+			conthread[i]=new Thread(new ConBroker(i));
+			conthread[i].start();
+		}
+		for(int i=0; i<consumers.size(); i++){
+			conthread[i].join();
+		}
+	}
+   public static class PubBroker implements Runnable{
+		int number_of_pub;
+		public PubBroker(int number_of_pub){
+			this.number_of_pub=number_of_pub;
+		}
+		public void run(){
+			try {
+				Socket requestSocket = new Socket(InetAddress.getByName("127.0.0.1"), 4321); //local address
 
-			try
-			{
-				// socket object to receive incoming client requests
-				socket = serverSocket.accept();
+				ObjectOutputStream out = new ObjectOutputStream(requestSocket.getOutputStream());
+				ObjectInputStream in = new ObjectInputStream(requestSocket.getInputStream());
 
-				System.out.println("A new client is connected : " + socket);
 
-				// obtaining input and out streams
-				DataInputStream in = new DataInputStream(socket.getInputStream());
-				DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-
-				System.out.println("Assigning new thread for this client");
-
-				// create a new thread object
-				Thread thread = new Thread(socket, in, out);
-
-				// Invoking the start() method
-				thread.start();
-
-			}
-			catch (Exception e){
-				socket.close();
+				String message = (String) in.readObject();
+				while (message != null) {
+					BusAndLocation TransformedMessage = (Suitmessage(message));
+					DataFromPublisher.add(TransformedMessage);//Suitmessage UNDONE
+					PutData(TransformedMessage);
+					message = (String) in.readObject();
+					if (!Keys.containsKey(TransformedMessage.GetBusLine())) Keys.put(TransformedMessage.GetBusLine(), TransformedMessage);
+				}
+				requestSocket.close();
+			}catch (IOException | ClassNotFoundException e) {
 				e.printStackTrace();
 			}
 		}
-	}
+   }
+   public static class ConBroker implements Runnable {
+	   int number_of_cons;
 
-	@Override
-	public void run() {
-		this.startBroker();
-		//We will get the data and we 'll decide if we want
-		//UNDONE
-	}
+	   public ConBroker(int number_of_cons) {
+		   this.number_of_cons = number_of_cons;
+	   }
 
-	private void startBroker() {
-		Socket requestSocket; //arxikopoihsh
-		ObjectOutputStream out;
-		ObjectInputStream in;
-		String message;
-		while (true) {
-			if (!recieveorsend)
-				try {
-					requestSocket = new Socket(InetAddress.getByName("127.0.0.1"), 4321); //local address
+	   public void run() {
+		   try {
+			   Socket requestSocket = new Socket(InetAddress.getByName("127.0.0.1"), 4321); //local address
 
-					out = new ObjectOutputStream(requestSocket.getOutputStream());
-					in = new ObjectInputStream(requestSocket.getInputStream());
+			   ObjectOutputStream out = new ObjectOutputStream(requestSocket.getOutputStream());
+			   ObjectInputStream in = new ObjectInputStream(requestSocket.getInputStream());
+
+			   out.writeObject("I am responsible for these keys:\n");
+			   String message = (String) in.readObject();
+
+			   for (String keys : Keys.keySet())
+				   out.writeObject(keys + "\n");
+			   //continue
+			   //THE OTHER BROKERS RESPONSIBLE FOR?
+			   int i = 0;
+			   for (Broker br : Node.getBrokers()) {
+				   if (gethashnumber() != br.gethashnumber()) {
+					   i++;
+					   out.writeObject("Keys responsible  broker: " + i + "\n");
+					   for (String keys : br.getKeys().keySet()) out.writeObject(keys + "\n");
+				   }
+			   }
+
+			   while (message != null) {
+				   out.writeObject("Bus from line " + message + "\n");
+				   for (BusAndLocation dr : DataResponsible) {
+					   if (dr.GetBusLine().equals(message))
+						   out.writeObject("Bus: " + dr.GetBusLine() + " location" + dr.GetLongitude() + " " + dr.GetLatitude() + "\n");
+				   }
+				   message = (String) in.readObject();
+
+			   }
+			   out.writeObject("That's all for now\n");
+			   requestSocket.close();
+			   out.close();
+			   in.close();
+		   } catch (IOException | ClassNotFoundException e) {
+			   e.printStackTrace();
+		   }
+	   }
+   }
 
 
-					message = (String) in.readObject();
-					while (message != null) {
-						BusAndLocation TransformedMessage = (Suitmessage(message));
-						DataFromPublisher.add(TransformedMessage);//Suitmessage UNDONE
-						PutData(TransformedMessage);
-						message = (String) in.readObject();
-						if (!Keys.containsKey(TransformedMessage.GetBusLine())) Keys.put(TransformedMessage.GetBusLine(), TransformedMessage);
-					}
-					requestSocket.close();
-				}catch (IOException | ClassNotFoundException e) {
-					e.printStackTrace();
-				}
-			else {
-				try {
-					requestSocket = new Socket(InetAddress.getByName("127.0.0.1"), 4321); //local address
-
-					out = new ObjectOutputStream(requestSocket.getOutputStream());
-					in = new ObjectInputStream(requestSocket.getInputStream());
-
-					out.writeObject("I am responsible for these keys:\n");
-					message = (String) in.readObject();
-
-					for (String keys : Keys.keySet())
-						out.writeObject(keys + "\n");
-					//continue
-					//THE OTHER BROKERS RESPONSIBLE FOR?
-					int i = 0;
-					for (Broker br : Node.getBrokers()) {
-						if (gethashnumber() != br.gethashnumber()) {
-							i++;
-							out.writeObject("Keys responsible  broker: " + i + "\n");
-							for (String keys : br.getKeys().keySet()) out.writeObject(keys + "\n");
-						}
-					}
-
-					while (message != null) {
-						out.writeObject("Bus from line " + message + "\n");
-						for (BusAndLocation dr : DataResponsible) {
-							if (dr.GetBusLine().equals(message))
-								out.writeObject("Bus: " + dr.GetBusLine() + " location" + dr.GetLongitude() + " " + dr.GetLatitude() + "\n");
-						}
-						message = (String) in.readObject();
-					}
-					out.writeObject("That's all for now\n");
-					requestSocket.close();
-					out.close();
-					in.close();
-				} catch (ClassNotFoundException | IOException e) {
-					e.printStackTrace();
-					System.err.println("data received in unknown format");
-				}
-			}
-		}
-	}
 
 	public static ArrayList<BusAndLocation> GetDataFromPublisher(){
     	 return  DataFromPublisher;
 	}
 
-	private String MD5(String md5) {
+	private static String MD5(String md5) {
 		try {
 			java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
 			byte[] array = md.digest(md5.getBytes());
@@ -181,13 +167,13 @@ public class Broker implements Runnable{
 		return null;
 	}
 
-	private void PutData(BusAndLocation Data){//UNDONE
+	private static void PutData(BusAndLocation Data){//UNDONE
 		String MD = MD5(Data.GetBusLine());
 		int mod=hashnumber;
 		if (MD != null && mod % 100 <= hashnumber && MD.equals(hashstring)) DataResponsible.add(Data);
 	}
 
-	private BusAndLocation Suitmessage(String message){//UNDONE
+	private static BusAndLocation Suitmessage(String message){//UNDONE
 		BusAndLocation BAL = new BusAndLocation();
 		//BAL.SetTopic(message.charAt(index));//SEE
 		//BAL.SetValue(Integer.parseInt(message.charAt(index)));
