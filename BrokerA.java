7cb6050329d820b1d682c77c6a671bbeff8acb1c
DrazorV <vagelisp.97@gmail.com>
2019-04-05 12:28:01 +0300

    Broker wont explose from exceptions any more, he will pause and wait for the server and consumer to connect to him

--- BrokerA.java
+++ BrokerA.java
@@ -48,59 +48,50 @@ public  class BrokerA {
         void startCommunication() throws IOException, ClassNotFoundException {
             while (true) {
                 System.out.println("THE CLIENT" + " " + connected.getInetAddress() + ":" + connected.getPort() + " IS CONNECTED ");
-
                 PrintWriter outToClient = new PrintWriter(connected.getOutputStream(), true);
+                BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connected.getInputStream()));
 
-                while(true){
-                    BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connected.getInputStream()));
+                hashed = BroUtilities.MD5(topics);
 
+                outToClient.println("I am broker A and I am responsible for these keys");
 
-                    hashed = BroUtilities.MD5(topics);
+                for (Topic topic : hashed.get("BrokerA")) outToClient.println(topic.getLineId());
 
-                    outToClient.println("I am broker A and I am responsible for these keys");
+                outToClient.println("Broker B is responsible for these Keys");
+                for (Topic topic : hashed.get("BrokerB")) outToClient.println(topic.getLineId());
 
-                    for (Topic topic : hashed.get("BrokerA")) {
-                        outToClient.println(topic.getLineId());
+                outToClient.println("Broker C is responsible for these Keys");
+                for (Topic topic : hashed.get("BrokerC")) outToClient.println(topic.getLineId());
+
+                outToClient.println("Done");
+
+                String inputLineId = inFromClient.readLine();
+                ArrayList<Value> values;
+
+                boolean temp = true;
+                while (temp) {
+                    try (Socket clientSocket = new Socket("localhost", 10000)) {
+                        ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream());
+                        out.writeObject("BrokerA");
+                        input = new BroUtilities().pull(clientSocket);
+                        temp = false;
+                    } catch (ConnectException e) {
+                        System.out.println("Waiting for Publisher!");
                     }
-
-                    outToClient.println("Broker B is responsible for these Keys");
-                    for (Topic topic : hashed.get("BrokerB")) outToClient.println(topic.getLineId());
-
-                    outToClient.println("Broker C is responsible for these Keys");
-                    for (Topic topic : hashed.get("BrokerC")) outToClient.println(topic.getLineId());
-
-                    outToClient.println("Done");
-
-                    String inputLineId = inFromClient.readLine();
-                    ArrayList<Value> values;
-
-                    boolean temp = true;
-                    while (temp) {
-                        try (Socket clientSocket = new Socket("localhost", 10000)) {
-                            ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream());
-                            out.writeObject("BrokerA");
-                            input = new BroUtilities().pull(clientSocket);
-                            temp = false;
-                        } catch (ConnectException e) {
-                            System.out.println("Waiting for Publisher!");
-                        }
-                    }
-
-
-                    try {
-                        for(Topic topic:input.keySet()){
-                            if(topic.getLineId().equals(inputLineId)){
-                                values = input.get(topic);
-                                values.sort(Comparator.comparing(o -> o.getBus().getTime()));
-                                for (Value bus_2_ : values) outToClient.println("The bus with id " + bus_2_.getBus().getVehicleId() + " was last spotted at [" + bus_2_.getBus().getTime() + "] at \nLatitude: " + bus_2_.getLatitude() + "\nLongitude: " + bus_2_.getLongitude() + "\nRoute: " + bus_2_.getBus().getLineName() + "\n-----------------------------------------------------------\n") ;
-                            }
-                        }
-                    }catch(NullPointerException e){
-                        outToClient.println("No values for me");
-                    }
-                    outToClient.println("next");
-
                 }
+
+                try {
+                    for(Topic topic:input.keySet()){
+                        if(topic.getLineId().equals(inputLineId)){
+                            values = input.get(topic);
+                            values.sort(Comparator.comparing(o -> o.getBus().getTime()));
+                            for (Value bus_2_ : values) outToClient.println("The bus with id " + bus_2_.getBus().getVehicleId() + " was last spotted at [" + bus_2_.getBus().getTime() + "] at \nLatitude: " + bus_2_.getLatitude() + "\nLongitude: " + bus_2_.getLongitude() + "\nRoute: " + bus_2_.getBus().getLineName() + "\n-----------------------------------------------------------\n") ;
+                        }
+                    }
+                }catch(NullPointerException e){
+                    outToClient.println("No values for me");
+                }
+                outToClient.println("next");
             }
         }
     }
